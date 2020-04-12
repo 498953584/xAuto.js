bc91894be8c11f67d2e379f5a8c53a667d650ef0
hexiong <hex06@mingyuanyun.com>
2020-04-12 18:45:58 +0800

    星钻
    三星星钻相关脚本

---
+++ sDiamonds/3三星星钻.js
@@ -0,0 +1,253 @@
+"auto";
+if (!requestScreenCapture()) {
+    toast("请求截图失败");
+    exit();
+}
+// sleep(500);//请求成功权限后，需暂停一下，直接提示单选款则打包apk会闪退
+
+setScreenMetrics(1080, 2280);
+main();
+
+function main() {
+    var num = dialogs.singleChoice("请选择执行动作", ["星钻乐园", "星钻助手（需安装对应软件）", "我的奖品领取"], 0);
+    switch (num) {
+        case 0:
+            sPark();
+            break;
+        case 1:
+            sDiamondTool();
+            break;
+        case 2:
+            CheckReward();
+            break;
+    }
+}
+
+function sPark() {
+    toastLog("请进入星钻乐园！");
+    id("lifeservice_actionbar_title_text").text("星钻乐园").waitFor();
+    var complete = [];
+    var ljlq = images.read("./Spark/ljlq.png");
+    var wl = images.read("./Spark/wl.png");
+    var grasp = images.read("./Spark/grasp.png");
+    var close3 = images.read("./Spark/close3.png");
+    var close1 = images.read("./Spark/close1.png");
+    var close2 = images.read("./Spark/close2.png");
+    var clickme = images.read("./Spark/click.png");
+    var grasp2 = images.read("./Spark/grasp2.png");
+    var cck = images.read("./Spark/cck.png");
+    var ccy = images.read("./Spark/ccy.png");
+    var cj = images.read("./Spark/cj.png");
+    var lastTime = null;
+    toastLog("ok");
+    while (true) {
+        let scr = captureScreen();
+        if (text("乐享福利 玩赚星钻").exists()) {
+            for (let child of idContains("box12").findOne().children()) {
+                let txt = child.text();
+                if (complete.indexOf(txt) == -1 && child.indexInParent() > 5 && child.clickable()) {
+                    log("进入游戏：" + txt);
+                    child.click();
+                    complete.push(txt);
+                    sleep(3500);
+                    break;
+                }
+                if (complete.length == 6) {
+                    toastLog("全部领取完成！");
+                    exit();
+                }
+                sleep(200);
+            }
+        } else if (clickImg(scr, ljlq)) {
+            log("中奖了" + textMatches(/\d*星钻/).findOne().text() + "，开始领星钻!");
+            sleep(1500);
+            textMatches(/领取成功，订单详情请查看|正在火速处理中，查看进度请点击/).waitFor();
+            while (!clickImg(captureScreen(), close3)) {
+                sleep(200);
+            };
+        } else if (clickImg(scr, wl)) {
+            log("领完了");
+            back();
+            sleep(500);
+        } else if (clickImg(scr, grasp) || clickImg(scr, cj) || clickImg(scr, clickme)) {
+            //log("抽红包");
+            sleep(100);//过快点击容易点入广告
+        } else if (clickImg(scr, grasp2)) {
+            //log("娃娃机");
+            sleep(1000);
+        } else if (clickImg(scr, cck, true)) {
+            //log("猜猜乐");
+            click(70, 1800);
+            sleep(1000);
+        } else if (clickImg(scr, ccy, true)) {
+            //log("猜成语");
+            click(250, 1700);
+            sleep(1000);
+
+        } else if (clickImg(scr, close1) || clickImg(scr, close2)) {
+            //log("关闭");
+            let fq = text("放弃奖励").findOne(1000);
+            if (fq) {
+                fq.click();
+            }
+            sleep(500);
+        } else {
+            //log("未识别界面!!");
+            if (lastTime == null) {
+                lastTime = new Date();
+            } else {
+                let d = new Date().getTime() - lastTime.getTime();
+                if (d > 10000) {
+                    toastLog("在未识别界面超过10秒，自动返回");
+                    back();
+                    lastTime = null;
+                }
+            }
+            continue;
+        }
+        sleep(100);
+        lastTime = null;
+    }
+}
+
+
+function CheckReward() {
+    toastLog("请进入我的奖励界面！");
+    id("lifeservice_actionbar_title_text").text("星钻乐园").waitFor();
+    idContains("db-content").waitFor();
+    var close3 = images.read("./Spark/close3.png");
+    var ljlq = images.read("./Spark/ljlq.png");
+    while (true) {
+        let unclaimed = text("待领取").findOne(1000);
+        if (unclaimed) {
+            unclaimed.parent().click();
+            sleep(200);
+            while (!click("去领取"));
+            sleep(2000);
+            while (!clickImg(captureScreen(), ljlq)) {
+                sleep(200);
+            }
+            log("中奖了" + textMatches(/\d*星钻/).findOne().text() + "，开始领星钻!");
+            sleep(1500);
+            text("领取成功，订单详情请查看").waitFor();
+            while (!clickImg(captureScreen(), close3)) {
+                sleep(200);
+            }
+        }
+        className("android.webkit.WebView").text("我的奖品").findOne().scrollForward();;
+        sleep(100);
+        if (className("android.view.View").text("没有更多了！").exists()) {
+            toastLog("奖品检查完成！");
+            exit();
+        }
+    }
+}
+
+function Convert(type) {
+    switch (type) {
+        case "我爱猜成语":
+            break;
+        case "天天抽红包":
+            break;
+        case "寻宝赢大奖":
+            break;
+        case "好运猜猜乐":
+            break;
+        case "开心娃娃机":
+            break;
+        case "幸运扭蛋机":
+            break;
+    }
+
+}
+
+function Playing(type) {
+    text("乐享福利 玩赚星钻").waitFor();
+    for (let child of idContains("box12").findOne().children()) {
+        let txt = child.text();
+        if (child.indexInParent() > 5 && child.clickable()) {
+            log("进入游戏：" + txt);
+            child.click();
+            sleep(3500);
+
+        }
+        sleep(200);
+    }
+}
+
+function sDiamondTool() {
+    launchApp("星钻助手");
+    for (let child of id("root").findOne().children()) {
+        let txt = child.text();
+        log(txt);
+        //if(txt!="三星浏览器能赠能花签到"){continue;}
+        child.click();
+        if (txt.indexOf("签到") > -1) {
+            log("签到");
+            sleep(1500);
+            if (txt.indexOf("三星浏览器") > -1) {
+                log("浏览器签到");
+                let choiceBrowser = text("三星浏览器").findOne(1000);
+                if (choiceBrowser) {
+                    choiceBrowser.parent().click();
+                }
+                let img = images.read("sqiandao.png");
+                let yimg = images.read("syiqiandao.png");
+                while (true) {
+                    let scr = captureScreen();
+                    clickImg(scr, sign);
+                    if (findImage(scr, ysign, { threshold: 0.8 })) {
+                        log("已签到");
+                        sleep(500);
+                        break;
+                    }
+                    sleep(200);
+                }
+            } else if (text("签到聚合页").exists()) {
+                log("签到礼包");
+                sleep(1000);
+                let img = className("android.view.View").text("0").findOne().parent().parent();
+                img.children().find(className("android.widget.Image")).forEach(function (c) {
+                    c.click();
+                    sleep(500);
+                });
+            } else {
+                textMatches(/签到成功.*/).waitFor();
+            }
+            sleep(500);
+            back();
+            sleep(500);
+        } else {
+            sleep(2500);
+            textMatches(/我要助力|今日已助力/).findOne().click();
+            //toastLog("ggg");
+            sleep(500);
+            textMatches(/助力成功.*|活动已结束/).waitFor();
+            //toastLog("ss");
+            id("lifeservice_menu_close").findOne().click();
+            sleep(1500);
+        }
+    }
+}
+
+
+/**
+ * 图中找图，并点击
+ * @param {源图} scr 
+ * @param {待查找的目标图} img 
+ * @param {是否点击，默认点击，true不点击} bc 
+ */
+function clickImg(scr, img, bc) {
+    //截图并找图
+    var p = findImage(scr, img, {
+        threshold: 0.9
+    });
+    if (p) {
+        if (!bc) {
+            click(p.x + img.getWidth() / 2, p.y + img.getHeight() / 2);
+        }
+        return true;
+    } else {
+        return false;
+    }
+}
